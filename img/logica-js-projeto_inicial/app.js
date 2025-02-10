a538370edde202756944df32ebe886c763d497e2
Dayane Bonfim <dayane.bonfim@live.com>
2025-02-10 20:51:08 -0300

    projeto inicial

---
+++ img/logica-js-projeto_inicial/app.js
@@ -0,0 +1,18 @@
+    // Crie uma lista vazia, com o nome listaGenerica.
+    let listaGenerica = [];
+
+    // Crie uma lista de linguagens de programação chamda linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++','Kotlin' e 'Phyton'.
+    let linguagensDeProgramacao = ['JavaScript','C','C++','Kotlin','Phyton'];
+
+    // Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java','Ruby' e 'Golang'.
+    linguagensDeProgramacao.push('Java','Ruby','Golang');
+
+    // Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+    nome = ['JavaScript','Phyton','Go'];
+    console.log(nome[0]);
+
+    // Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+    console.log(nome[1]);
+
+    // Crie uma lista com 3 nomes e exiba no console apenas o terceiro elemento.
+    console.log(nome[2]);
